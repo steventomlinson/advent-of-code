ec6800a848d11347089a2afad693ade35926836b
Steven Tomlinson <t97steven@gmail.com>
2018-12-01 21:40:04 +0000

    Updated to use IntSets because they're even faster!!!

--- day1/ChronalCalibration.hs
+++ day1/ChronalCalibration.hs
@@ -1,28 +1,28 @@
 module ChronalCalibration where
 
-import Data.Set (Set)
-import qualified Data.Set as Set
+import Data.IntSet (IntSet)
+import qualified Data.IntSet as IntSet
 
-inputToList :: String -> [Integer]
+inputToList :: String -> [Int]
 inputToList s = map read $ words (filter (not . flip elem ",'+") s)
 
-part1_solve :: String -> Integer
+part1_solve :: String -> Int
 part1_solve = sum . inputToList
 
-part1_solveFromFile :: String -> IO Integer
+part1_solveFromFile :: String -> IO Int
 part1_solveFromFile s = do 
     input_data <- readFile s
     return (part1_solve input_data)
 
-part2_solve :: String -> Integer
-part2_solve = inner_solve 0 (Set.fromList []) . (cycle . inputToList)
+part2_solve :: String -> Int
+part2_solve = inner_solve 0 (IntSet.fromList []) . (cycle . inputToList)
     where 
-        inner_solve :: Integer -> Set Integer -> [Integer] -> Integer
+        inner_solve :: Int -> IntSet -> [Int] -> Int
         inner_solve curr_item items_seen (x:xs) 
-            | curr_item `Set.member` items_seen = curr_item
-            | otherwise = inner_solve (curr_item + x) (curr_item `Set.insert` items_seen) xs
+            | curr_item `IntSet.member` items_seen = curr_item
+            | otherwise = inner_solve (curr_item + x) (curr_item `IntSet.insert` items_seen) xs
 
-part2_solveFromFile :: String -> IO Integer
+part2_solveFromFile :: String -> IO Int
 part2_solveFromFile s = do
     input_data <- readFile s
     return (part2_solve input_data)
