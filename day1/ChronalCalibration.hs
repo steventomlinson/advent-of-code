0bda0a9562530b2cbe6bddcd690e3a6937e3d4f4
Steven Tomlinson <t97steven@gmail.com>
2018-12-01 20:36:24 +0000

    Now using the cycle function

--- day1/ChronalCalibration.hs
+++ day1/ChronalCalibration.hs
@@ -12,13 +12,9 @@ part1_solveFromFile s = do
     return (part1_solve input_data)
 
 part2_solve :: String -> Integer
-part2_solve s = let init_xs = inputToList s in 
-    let inner_solve trace xs = case xs of 
-                                [] -> inner_solve trace init_xs
-                                (x:xs') 
-                                    |  (x + head trace) `elem` trace -> x + head trace
-                                    | otherwise-> inner_solve ((x + head trace) : trace) xs'
-    in inner_solve [0] init_xs
+part2_solve s = let inner_solve items_seen (x:xs)   |  (x + head items_seen) `elem` items_seen = x + head items_seen
+                                                    | otherwise = inner_solve ((x + head items_seen) : items_seen) xs
+    in inner_solve [0] (cycle (inputToList s))
 
 part2_solveFromFile :: String -> IO Integer
 part2_solveFromFile s = do
