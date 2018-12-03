17ea3f2152155e5b157c249983af5ba23223f7ef
Steven Tomlinson <t97steven@gmail.com>
2018-12-03 18:53:44 +0000

    Fixed some hlint errors

--- day3/NoMatterHowYouSliceIt.hs
+++ day3/NoMatterHowYouSliceIt.hs
@@ -3,18 +3,19 @@ module NoMatterHowYouSliceIt where
 import Data.Char
 import qualified Data.Map as Map
 
-convert_input :: [String ] -> [(Int, (Int, Int, Int, Int))]
-convert_input [] = []
-convert_input (x:xs) = (to_tuple . map (read :: String->Int) . words . map (\c -> if isNumber c then c else ' ')) x : convert_input xs
+convertInput :: [String ] -> [(Int, (Int, Int, Int, Int))]
+convertInput [] = []
+convertInput (x:xs) = (to_tuple . map (read :: String->Int) . words . map (\c -> if isNumber c then c else ' ')) x : convertInput xs
     where 
         to_tuple [a,b,c,d,e] = (a, (b, c , d -1, e -1))
+        to_tuple _ = error "invalid input"
 
-get_coords :: (Int, (Int, Int, Int, Int)) -> [(Int, Int)]
-get_coords (_,(x,y,w,h)) = [(a,b) | a <- [x..w+x], b <- [y..h+y]]
+getCoords :: (Int, (Int, Int, Int, Int)) -> [(Int, Int)]
+getCoords (_,(x,y,w,h)) = [(a,b) | a <- [x..w+x], b <- [y..h+y]]
 
 fillAllClaims :: Map.Map (Int, Int) Int -> [(Int, (Int, Int, Int, Int))] -> Map.Map (Int, Int) Int
 fillAllClaims m [] = m
-fillAllClaims m (c:cs) = fillAllClaims (fill_claim (get_coords c) m) cs 
+fillAllClaims m (c:cs) = fillAllClaims (fill_claim (getCoords c) m) cs 
     where
         fill_claim :: [(Int, Int)] -> Map.Map (Int, Int) Int -> Map.Map (Int, Int) Int
         fill_claim [] m' = m'
@@ -23,15 +24,15 @@ fillAllClaims m (c:cs) = fillAllClaims (fill_claim (get_coords c) m) cs
 
 
 part1_solve :: String -> Int
-part1_solve = Map.size . Map.filter (>1) . fillAllClaims Map.empty . convert_input . lines
+part1_solve = Map.size . Map.filter (>1) . fillAllClaims Map.empty . convertInput . lines
 
 part2_solve :: String -> Int
 part2_solve s = findValid (Map.filter (==1) (fillAllClaims Map.empty cs)) cs
     where
-        cs = (convert_input . lines) s
+        cs = (convertInput . lines) s
         findValid :: Map.Map (Int, Int) Int -> [(Int, (Int, Int, Int, Int))] -> Int
         findValid _ [] = error "No valid Id"
-        findValid m (c:cs') | (all (`Map.member` m) . get_coords) c = fst c
+        findValid m (c:cs') | (all (`Map.member` m) . getCoords) c = fst c
                             | otherwise = findValid m cs'
 
 solveFromFile :: String -> (String -> a) -> IO a
