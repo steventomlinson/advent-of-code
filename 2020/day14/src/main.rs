ebce1574e8fbc47c408dac0326aab97366b5da14
Steven Tomlinson <t97steven@gmail.com>
2020-12-24 13:45:22 +0000

    day 14 init

---
+++ 2020/day14/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let args : Vec<String> = std::env::args().collect();
+    assert_eq!(args.len(), 2);
+    let filename : &String = &args[1];
+    let contents : String = std::fs::read_to_string(filename)
+        .expect("Unable to read file!");
+}
